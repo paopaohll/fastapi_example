fe52a8b9a718dde5f416a077e0c9f846fffde4f2
paopaohll <paopaohll@163.com>
2024-04-29 15:21:26 +0800

    add utils.py

---
+++ utils.py
@@ -0,0 +1,21 @@
+from typing import List
+from typing import Tuple
+from typing import Set
+
+
+def process_list(items: List[str]):
+    for item in items:
+        print(item.title())
+
+
+def process_tuple(items_t: Tuple[int, int, str], items_s: Set[str]):
+    pass
+
+
+if __name__ == "__main__":
+    fruit_list = ["apple", "banana", "watermelon"]
+    process_list(items=fruit_list)
+
+    t = ("apple", "banana", "watermelon")
+    print(t.index(1))
+    print(t)
