4f562da1572ded59a9eebd3d897847425dafe8db
paopaohll <paopaohll@163.com>
2024-04-29 18:02:46 +0800

    add learn fastapi

--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ from typing import List
 from typing import Tuple
 from typing import Set
 
+import dis
+
 
 def process_list(items: List[str]):
     for item in items:
@@ -12,9 +14,22 @@ def process_tuple(items_t: Tuple[int, int, str], items_s: Set[str]):
     pass
 
 
-if __name__ == "__main__":
-    fruit_list = ["apple", "banana", "watermelon"]
-    process_list(items=fruit_list)
+def add():
+    global n
+    n = n + 1
 
-    t = ("apple", "banana", "watermelon")
-    print(t)
+
+def foo():
+    lst.sort()
+
+
+if __name__ == "__main__":
+    # fruit_list = ["apple", "banana", "watermelon"]
+    # process_list(items=fruit_list)
+
+    # t = ("apple", "banana", "watermelon")
+    # print(t)
+    # n = 0
+    # print(dis.dis(add))
+    lst = [4, 1, 3, 2]
+    print(dis.dis(foo))
