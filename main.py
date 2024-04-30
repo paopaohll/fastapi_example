9d4507d8ed0c1bdaa9e5c76835e6c13cffef2f65
paopaohll <paopaohll@163.com>
2024-04-30 16:47:23 +0800

    ADD 学习查询参数和字符串校验

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    """Docstring for Item."""
+
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
 
 
 app = FastAPI()
@@ -14,3 +25,29 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.get("/items/")
+def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=10)):
+    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.post("/items/")
+def create_item(item: Item):
+    item_dict = item.model_dump()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+        return item_dict
+    return item
+
+
+@app.put("/items/{item_id}")
+def update_item2(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
