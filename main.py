267bbf89bced13046c52579bd3cd96b217fda1ab
paopaohll <paopaohll@163.com>
2024-05-06 19:37:20 +0800

    sync

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from typing import Union
+from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 
 from pydantic import BaseModel
 
@@ -14,40 +14,51 @@ class Item(BaseModel):
     tax: float | None = None
 
 
+class User(BaseModel):
+    username: str
+    full_name: str | None = None
+
+
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
-@app.get("/items/")
-def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=10)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
+# @app.get("/items/")
+# def read_items(q: Union[str, None] = Query(default=None, min_length=3, max_length=10)):
+#     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
+#     if q:
+#         results.update({"q": q})
+#     return results
+
+
+# @app.post("/items/")
+# def create_item(item: Item):
+#     item_dict = item.model_dump()
+#     if item.tax:
+#         price_with_tax = item.price + item.tax
+#         item_dict.update({"price_with_tax": price_with_tax})
+#         return item_dict
+#     return item
+
+
+# @app.put("/items/{item_id}")
+# def update_item2(item_id: int, item: Item, q: str | None = None):
+#     result = {"item_id": item_id, **item.model_dump()}
+#     if q:
+#         result.update({"q": q})
+#     return result
+
+
+@app.put("/items/{item_id}/")
+def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
     return results
-
-
-@app.post("/items/")
-def create_item(item: Item):
-    item_dict = item.model_dump()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-        return item_dict
-    return item
-
-
-@app.put("/items/{item_id}")
-def update_item2(item_id: int, item: Item, q: str | None = None):
-    result = {"item_id": item_id, **item.model_dump()}
-    if q:
-        result.update({"q": q})
-    return result
