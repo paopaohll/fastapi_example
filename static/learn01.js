3ba485903835684442a99561069c3b3554b0642d
paopaohll <paopaohll@163.com>
2024-05-06 16:57:44 +0800

    add gitignore

--- static/learn01.js
+++ static/learn01.js
@@ -1,2 +1,12 @@
-var hello = "Hello World"
-console.log(hello)
+// 定义变量hell0,
+var hello = "Hello World";
+/*
+打印变量hello
+编写if判断语句
+*/
+console.log(hello);
+if (2 > 1) {
+    x = 1;
+    y = 2;
+    z = 3;
+}
